9035fdbba5d3806ae274e2d61a2361c607328e73
Filip Rydberg <fr222cy@student.lnu.se>
2016-10-10 13:44:54 +0200

    Connected Components

--- src/fr222cy/MyConnectedComponents.java
+++ src/fr222cy/MyConnectedComponents.java
@@ -4,8 +4,13 @@
 package fr222cy;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import javax.xml.transform.Templates;
 
 import fr222cy_assign3.graphs.ConnectedComponents;
+import fr222cy_assign3.graphs.DFS;
 import fr222cy_assign3.graphs.DirectedGraph;
 import fr222cy_assign3.graphs.Node;
 
@@ -17,8 +22,41 @@ public class MyConnectedComponents<E> implements ConnectedComponents<E> {
 
 	@Override
 	public Collection<Collection<Node<E>>> computeComponents(DirectedGraph<E> dg) {
-		// TODO Auto-generated method stub
-		return null;
+		HashSet<Node<E>> visited = new HashSet<>();
+		HashSet<Collection<Node<E>>> setOfCollections = new HashSet<>();
+		Collection<Node<E>> collection;
+		Collection<Node<E>> temp;
+		Iterator<Node<E>> nodeIterator = dg.iterator();
+		boolean connection;
+		MyDFS<E> dfs = new MyDFS<>();
+		while(nodeIterator.hasNext()){
+			connection = false;
+			Node<E> current = nodeIterator.next();
+			if(!visited.contains(current)){
+				collection =  dfs.dfs(dg, current);
+
+				for(Node<E> node : collection){
+					if(visited.contains(node)){
+						for(Collection<Node<E>> coll : setOfCollections){
+							if(coll.contains(node)){
+								visited.addAll(collection);
+								coll.addAll(collection);
+								connection = true;
+							}
+						}
+					}	
+				}
+
+				if(!connection){
+					visited.addAll(collection);
+					temp = new HashSet<>();
+					temp.addAll(collection);
+					setOfCollections.add(temp);
+				}
+			}
+		}
+
+		return setOfCollections;
 	}
 
 }
